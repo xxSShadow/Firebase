96b2b278449112cd948085a471470c3cdccd9e12
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-03-27 15:33:54 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const Account = require('./AccountKey.json');
+Account = require('./AccountKey.json');
 initializeApp({ credential: cert(Account) });
 Database = getFirestore();
 Collection = Database.collection('MEMBERS');
