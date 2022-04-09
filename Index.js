d987642649a9c539d7b12a37ca09e25a8ba6a548
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-04-09 13:05:07 -0700

    Delete Index.js

--- Index.js
+++
@@ -1,8 +0,0 @@
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-Account = require('./AccountKey.json');
-initializeApp({ credential: cert(Account) });
-Database = getFirestore();
-Collection = Database.collection('GUILDS');
-Collection.doc('IDS');
-Collection.set({ NAME: 'TESTING' });
